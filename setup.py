d35278aa67e3e9c85520d1d1b66a035a4a341a7a
Ryan Saplan <Ryan.Saplan@albertsons.com>
2023-10-20 19:50:59 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='property-file-loader',
+    version='1.0',
+    description='Python script that loads a property file into a Class\' dictionary attribute.',
+    packages=['util'],
+    author='Ryan L. Saplan',
+    author_email='ryanlsaplan@gmail.com'
+)
