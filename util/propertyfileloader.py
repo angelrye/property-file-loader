d5f65496f4ad1ecdb8c04977faadb711c0d72c0e
Ryan Saplan <Ryan.Saplan@albertsons.com>
2023-10-26 10:10:53 +0800

    added public functions.

--- util/propertyfileloader.py
+++ util/propertyfileloader.py
@@ -6,16 +6,12 @@ class PropertyFileLoader:
 
     def __init__(self, file):
         '''
-        Accepts a Text File object 
+        Accepts a Text File object or filename 
         '''
-        self.__file__ = file
-        self.properties = dict()
-        self.__load_properties__()
+        self.__properties__ = dict()
 
-    def __load_properties__(self):
-
-        def yield_line():
-            for line in self.__file__:
+        def yield_line(file):
+            for line in file:
                 if (line in ['\n', '\n\r']) or (len(line.strip()) == 0) or (line.strip()[0] == '#'):
                     continue
 
@@ -28,12 +24,29 @@ class PropertyFileLoader:
             key, value = text.split('=')
             return {f'{key.strip()}': f'{value.strip()}'}
 
-        if not (self.__file__):
-            raise RuntimeError('PropertyFileLoader class was not initialized.')
+        def update_property(properties):
+            for key_value in yield_line(properties):
+                self.__properties__.update(new_dict(key_value))
 
-        if isinstance(self.__file__, io.TextIOWrapper):
+        if isinstance(file, str):
+            with open(file) as prop:
+                update_property(prop)
 
-            for key_value in yield_line():
-                self.properties.update(new_dict(key_value))
+        elif isinstance(file, io.TextIOWrapper):
+            update_property(file)
         else:
             raise TypeError('Invalid file')
+
+    def getpropertyvalue(self, key):
+        value = self.__properties__.get(key)
+
+        if not value:
+            raise ValueError('Property is not defined.')
+
+        return value
+
+    def iskeyexist(self, key):
+        return key in self.__properties__
+
+    def getproperties(self):
+        return self.__properties__
--- util/propertyfileloader_test.py
+++ util/propertyfileloader_test.py
@@ -12,7 +12,17 @@ def test():
     with open(path) as file:
         propertyfileloader = PropertyFileLoader(file)
 
-    print(propertyfileloader.properties)
+    print(propertyfileloader.__properties__)
+
+    print(
+        f"is Property 1 configured? {propertyfileloader.iskeyexist('property1')}")
+    print(
+        f"is Property 2 configured? {propertyfileloader.iskeyexist('property2')}")
+
+    print(
+        f"Value of property1 is {propertyfileloader.getpropertyvalue('property1')}")
+    print(
+        f"Value of property1 is {propertyfileloader.getpropertyvalue('property2')}")
 
 
 if __name__ == '__main__':
